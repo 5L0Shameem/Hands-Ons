dc9f3e73a9a830fcf8d373d90354fe5982c9ba78
5L0Shameem <133978603+5L0Shameem@users.noreply.github.com>
2026-03-15 08:02:26 -0700

    Add files via upload

---
+++ Week 4/Day 3/EmployeeBonus_calculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class Q3
+{
+    static void Main()
+    {
+        Console.Write("Enter Employee Name: ");
+        string name = Console.ReadLine();
+
+        Console.Write("Enter Salary: ");
+        double salary = double.Parse(Console.ReadLine());
+
+        Console.Write("Enter Experience (years): ");
+        double experience = double.Parse(Console.ReadLine());
+
+        double bonusPercentage = (experience < 2) ? 0.05 :
+                                 (experience <= 5) ? 0.10 : 0.15;
+
+        double bonus = salary * bonusPercentage;
+        double finalSalary = salary + bonus;
+
+        Console.WriteLine("Employee: " + name);
+        Console.WriteLine("Bonus: " + bonus);
+        Console.WriteLine("Final Salary: " + finalSalary);
+    }
+}
