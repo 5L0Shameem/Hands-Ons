9f7802291652536b1a5dc25cce7eb3598ec6203f
5L0Shameem <133978603+5L0Shameem@users.noreply.github.com>
2026-03-27 08:16:55 -0700

    Add files via upload

---
+++ Week 5/Day 22/BankAccount.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+class InsufficientBalanceException : Exception
+{
+    public InsufficientBalanceException(string message) : base(message) { }
+}
+
+class BankAccount
+{
+    private double balance;
+
+    public BankAccount(double balance)
+    {
+        this.balance = balance;
+    }
+
+    public void Withdraw(double amount)
+    {
+        if (amount > balance)
+        {
+            throw new InsufficientBalanceException("Error: Withdrawal amount exceeds available balance");
+        }
+        else
+        {
+            balance -= amount;
+            Console.WriteLine("Withdrawal successful. Remaining Balance = " + balance);
+        }
+    }
+}
+
+class BankApp
+{
+    static void Main()
+    {
+        Console.Write("Enter Balance: ");
+        double bal = double.Parse(Console.ReadLine());
+
+        Console.Write("Enter Withdraw Amount: ");
+        double amt = double.Parse(Console.ReadLine());
+
+        BankAccount account = new BankAccount(bal);
+
+        try
+        {
+            account.Withdraw(amt);
+        }
+        catch (InsufficientBalanceException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            Console.WriteLine("Transaction completed");
+        }
+    }
+}
