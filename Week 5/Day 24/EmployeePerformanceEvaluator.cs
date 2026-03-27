4b8d8e5df202d86db30c6e43d7de89e8dfd57e98
5L0Shameem <133978603+5L0Shameem@users.noreply.github.com>
2026-03-27 08:22:49 -0700

    Add files via upload

---
+++ Week 5/Day 24/EmployeePerformanceEvaluator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class EmployeePerformanceEvaluator
+{
+    static (double, int) GetPerformanceData(double sales, int rating)
+    {
+        return (sales, rating);
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter Employee Name: ");
+        string name = Console.ReadLine();
+
+        double sales;
+        int rating;
+
+        while (true)
+        {
+            Console.Write("Enter Sales Amount: ");
+            if (double.TryParse(Console.ReadLine(), out sales) && sales >= 0)
+                break;
+            Console.WriteLine("Invalid Sales Amount");
+        }
+
+        while (true)
+        {
+            Console.Write("Enter Rating (1-5): ");
+            if (int.TryParse(Console.ReadLine(), out rating) && rating >= 1 && rating <= 5)
+                break;
+            Console.WriteLine("Invalid Rating");
+        }
+
+        var data = GetPerformanceData(sales, rating);
+
+        string performance = data switch
+        {
+            ( >= 100000, >= 4) => "High Performer",
+            ( >= 50000, >= 3) => "Average Performer",
+            _ => "Needs Improvement"
+        };
+
+        Console.WriteLine("\nEmployee Name: " + name);
+        Console.WriteLine("Sales Amount: " + data.Item1);
+        Console.WriteLine("Rating: " + data.Item2);
+        Console.WriteLine("Performance: " + performance);
+    }
+}
