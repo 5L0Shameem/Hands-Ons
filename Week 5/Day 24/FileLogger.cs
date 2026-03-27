aa74a47405d169fc51d70db0cc286befbbce889e
5L0Shameem <133978603+5L0Shameem@users.noreply.github.com>
2026-03-27 08:23:35 -0700

    Add files via upload

---
+++ Week 5/Day 24/FileLogger.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+class FileLogger
+{
+    static void Main()
+    {
+        Console.Write("Enter message: ");
+        string message = Console.ReadLine();
+
+        try
+        {
+            FileStream fs = new FileStream("log.txt", FileMode.Append, FileAccess.Write);
+            byte[] data = Encoding.UTF8.GetBytes(message + Environment.NewLine);
+
+            fs.Write(data, 0, data.Length);
+            fs.Close();
+
+            Console.WriteLine("Message written to file successfully");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+        finally
+        {
+            Console.WriteLine("Operation completed");
+        }
+    }
+}
