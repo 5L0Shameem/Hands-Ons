494e50d5b87afeb4b3492641b31a8c04cb732d2f
5L0Shameem <133978603+5L0Shameem@users.noreply.github.com>
2026-03-27 08:43:08 -0700

    Add files via upload

---
+++ Week 6/Day 25/AsyncFileLogger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+
+class AsyncFileLogger
+{
+    static async Task WriteLogAsync(string message)
+    {
+        await Task.Delay(1000);
+        Console.WriteLine("Log Written: " + message);
+    }
+
+    static async Task Main()
+    {
+        Task t1 = WriteLogAsync("User logged in");
+        Task t2 = WriteLogAsync("File uploaded");
+        Task t3 = WriteLogAsync("Error occurred");
+
+        Console.WriteLine("Logging in progress...");
+
+        await Task.WhenAll(t1, t2, t3);
+
+        Console.WriteLine("All logs completed");
+    }
+}
