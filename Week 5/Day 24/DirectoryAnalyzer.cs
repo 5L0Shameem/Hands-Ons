cddabdb4ad93a1c9910f21cdc49927f5caee823f
5L0Shameem <133978603+5L0Shameem@users.noreply.github.com>
2026-03-27 08:21:02 -0700

    Add files via upload

---
+++ Week 5/Day 24/DirectoryAnalyzer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+class DirectoryAnalyzer
+{
+    static void Main()
+    {
+        Console.Write("Enter root directory path: ");
+        string path = Console.ReadLine();
+
+        try
+        {
+            DirectoryInfo dir = new DirectoryInfo(path);
+
+            if (!dir.Exists)
+            {
+                Console.WriteLine("Invalid directory path");
+                return;
+            }
+
+            DirectoryInfo[] subDirs = dir.GetDirectories();
+
+            Console.WriteLine("\nDirectory Details:");
+
+            foreach (DirectoryInfo subDir in subDirs)
+            {
+                FileInfo[] files = subDir.GetFiles();
+                Console.WriteLine(subDir.Name + " - Files Count: " + files.Length);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+    }
+}
