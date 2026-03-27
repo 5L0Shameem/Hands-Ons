3725ff57fc4173e5f59e9687de12a6ef55ad970d
5L0Shameem <133978603+5L0Shameem@users.noreply.github.com>
2026-03-27 08:43:37 -0700

    Add files via upload

---
+++ Week 6/Day 25/DiscountCalculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class DiscountCalculator
+{
+    static void Main()
+    {
+        Console.Write("Enter Product Name: ");
+        string name = Console.ReadLine();
+
+        double price;
+        double discount;
+
+        while (true)
+        {
+            Console.Write("Enter Product Price: ");
+            if (double.TryParse(Console.ReadLine(), out price) && price >= 0)
+                break;
+            Console.WriteLine("Invalid Price");
+        }
+
+        while (true)
+        {
+            Console.Write("Enter Discount Percentage: ");
+            if (double.TryParse(Console.ReadLine(), out discount) && discount >= 0)
+                break;
+            Console.WriteLine("Invalid Discount");
+        }
+
+        double finalPrice = price - (price * discount / 100);
+
+        Console.WriteLine("\nProduct Name: " + name);
+        Console.WriteLine("Original Price: " + price);
+        Console.WriteLine("Discount: " + discount + "%");
+        Console.WriteLine("Final Price: " + finalPrice);
+    }
+}
