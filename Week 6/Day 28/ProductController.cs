45c392fc5f611e3990c3909261fb8aae760672f9
5L0Shameem <133978603+5L0Shameem@users.noreply.github.com>
2026-04-16 21:34:48 -0700

    Add files via upload

---
+++ Week 6/Day 28/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication3.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WebApplication3.Controllers
+{
+    public class ProductController : Controller
+    {
+        
+        static List<Product> products = new List<Product>()
+        {
+            new Product{ Id = 1, Name = "Laptop", Price = 50000 },
+            new Product{ Id = 2, Name = "Mobile", Price = 20000 },
+            new Product{ Id = 3, Name = "Headphones", Price = 2000 }
+        };
+
+        
+        public IActionResult Index()
+        {
+            return View(products);
+        }
+
+        
+        public IActionResult Details(int id)
+        {
+            var product = products.FirstOrDefault(p => p.Id == id);
+            return View(product);
+        }
+    }
+}
